790f50c67ea4ede354669f8c5282696b27c0fff8
Aushtiin <aotangban@gmail.com>
2021-06-18 13:24:46 +0100

    error at login

--- src/Components/LoginScreen.js
+++ src/Components/LoginScreen.js
@@ -3,13 +3,16 @@ import { Form, Row, Col, Container, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import Loader from './Loader'
+import Message from './Message'
 
 const LoginScreen = ({ history }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
 
   async function handleSubmit(e) {
+    try{
     e.preventDefault()
     setLoading(true)
     const config = {
@@ -21,11 +24,17 @@ const LoginScreen = ({ history }) => {
     localStorage.setItem('logindetails', JSON.stringify(data))
     setLoading(false)
     history.push('/products')
+  } catch (e) {
+    console.error(e)
+    setError('An error occured please.')
+    setLoading(false)
+  }
   }
   return (
     <Container className=''>
       <h1 className='my-3'>Sign In</h1>
       <Form onSubmit={handleSubmit}>
+      {error && <Message>{error}</Message>}
         <Form.Group>
           <Form.Label>Email</Form.Label>
           <Form.Control type='text' value={email} onChange={(e) => setEmail(e.target.value)} />
